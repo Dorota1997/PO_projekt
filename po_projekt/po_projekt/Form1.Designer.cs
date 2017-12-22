3874615914f3fe624b1d886ffcfdc888d18d41c4
Choocoholik <gildota@wp.pl>
2017-12-22 20:46:16 +0100

    Stworzenie projektu i menu z przyciskiem graj

---
+++ po_projekt/po_projekt/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace po_projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.play_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // play_button
+            // 
+            this.play_button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.play_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.play_button.Location = new System.Drawing.Point(189, 76);
+            this.play_button.Name = "play_button";
+            this.play_button.Size = new System.Drawing.Size(121, 46);
+            this.play_button.TabIndex = 0;
+            this.play_button.Text = "Graj";
+            this.play_button.UseVisualStyleBackColor = false;
+            this.play_button.Click += new System.EventHandler(this.play_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(521, 313);
+            this.Controls.Add(this.play_button);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projekt";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button play_button;
+    }
+}
+
---
+++ po_projekt/po_projekt/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace po_projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void play_button_Click(object sender, EventArgs e)
+        {
+            Form2 przejscie = new Form2();
+            this.Hide();                    // sposob aby ukryc biezace okno
+            przejscie.Show();               // sposob aby otworzyc okno z Form2
+        }
+    }
+}
