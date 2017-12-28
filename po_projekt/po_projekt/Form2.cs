d507a3e5efbedb635702ce308762ce8d54f03c89
Choocoholik <gildota@wp.pl>
2017-12-28 09:11:09 +0100

    Umieszczenie wlasciwych obrazkow do kolumn i wierszy

--- po_projekt/po_projekt/Form2.cs
+++ po_projekt/po_projekt/Form2.cs
@@ -19,11 +19,34 @@ namespace po_projekt
             "b", "b", "f", "f", "H", "H", "N", "N", "!", "!", "$", "$", "O", "O", "v", "v"
         };
 
+        Label pierwszy_klik, drugi_klik;
+
         public Form2()
         {
             InitializeComponent();
+            AssignIconsToSquares();
         }
 
+        private void AssignIconsToSquares()
+        {
+            Label label;
+            int randomNumer;
+
+            for(int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                {
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                }
+                else
+                    continue;
+
+                randomNumer = random.Next(0, ikonki.Count);
+                label.Text = ikonki[randomNumer];
+
+                ikonki.RemoveAt(randomNumer);
+            }
+        }
         
     }
 }
