e2b303fef7248dfe189de00fdb6fc0558c90a627
Choocoholik <gildota@wp.pl>
2018-01-05 17:26:24 +0100

    Jednak robię od nowa :(

--- po_projekt/po_projekt/Form2.cs
+++ po_projekt/po_projekt/Form2.cs
@@ -12,9 +12,67 @@ namespace po_projekt
 {
     public partial class Form2 : Form
     {
+        Random random = new Random();
+
+        List<string> ikonki = new List<string>()
+        {
+            "O", "O", "!", "!", "€", "€", "ł", "ł", "l", "l", "b", "b", "ń", "ń", "ó", "ó"
+        };
+
+        Label pierwsze_klikniecie, drugie_klikniecie;
+
         public Form2()
         {
             InitializeComponent();
+            obrazki_do_kwadraciokow();
+        }
+
+        private void Klik(object sender, EventArgs e)
+        {
+            Label KlikLabel = sender as Label;
+
+            if (KlikLabel == null)
+            {
+                return;
+            }
+
+            if (KlikLabel.ForeColor == Color.Black)
+            {
+                return;
+            }
+
+            if (pierwsze_klikniecie == null)
+            {
+                pierwsze_klikniecie = KlikLabel;
+                pierwsze_klikniecie.ForeColor = Color.Black;
+                return;
+            }
+
+
+        }
+
+        private void obrazki_do_kwadraciokow()
+        {
+            Label label;
+            int losowy_numer;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                {
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                }
+                else
+                {
+                    continue;
+                }
+
+                losowy_numer = random.Next(0, ikonki.Count);
+
+                label.Text = ikonki[losowy_numer];
+
+                ikonki.RemoveAt(losowy_numer);
+            }
         }
     }
 }
